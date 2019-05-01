b22104deae6805e6e2d5faa3842d6aa326d57657
marsovic <36446456+marsovic@users.noreply.github.com>
2019-05-01 19:56:20 +0200

    Add files via upload

--- creation_compte.php
+++ creation_compte.php
@@ -26,7 +26,7 @@
 				<li class="nav-item"><a class="nav-link" href="best-sellers.html">Best-Sellers</a></li>
 				<li class="nav-item"><a class="nav-link" href="recherche.html">Recherche</a></li>
 				<li class="nav-item"><a class="nav-link" href="vendre.html">Vendre</a></li>
-				<li class="nav-item"><a class="nav-link" href="compte.html">Mes Comptes</a></li>
+				<li class="nav-item"><a class="nav-link" href="preconnexion.php">Mes Comptes</a></li>
 				<li class="nav-item"><a class="nav-link" href="panier.html">Panier</a></li>
 			</ul>
 		</div>
@@ -35,38 +35,66 @@
 
 
 	<form action="connexion.php" method="post">
-			<h2>Création de votre compte compte</h2>
 			<div class="form-group row">
-				<label for="mail" class="col-md-1 col-form-label">Adresse e-mail</label>
+				<label for="mail" class="col-md-1 col-form-label"></label>
+				<h2>Création de votre compte ECE</h2>
+			</div>
+
+
+
+			<div class="form-group row">
+				<label class="col-md-1 col-form-label"></label>
+				<label for="mail" class="col-md-2 col-form-label">Adresse mail</label>
 				<div class="col-md-2">
 					<input type="text" class="form-control" name="mail" placeholder="@edu.ece.fr" />
+				</div>
+				<label class="col-md-1 col-form-label"></label>
+				<label for="image" class="col-md-1 col-form-label">Photo de profil</label>
+				<div class="col-md-2">
+					<input type="file" class="form-control" name="image"/>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-1 col-form-label"></label>
+				<label for="MDP" class="col-md-2 col-form-label">Mot de passe</label>
+				<div class="col-md-2">
+					<input type="text" class="form-control" name="MDP"/>
+				</div>
+
+				<label for="wallpaper" class="col-md-1 col-form-label"></label>
+				<label for="wallpaper" class="col-md-1 col-form-label">Photo de couverture</label>
+				<div class="col-md-2">
+					<input type="file" class="form-control" name="image"/>
+				</div>
+
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-1 col-form-label"></label>
+				<label for="nom" class="col-md-2 col-form-label" placeholder="Nom">Nom & Prénom</label>
+				<div class="col-md-1">
+					<input type="text" class="form-control" name="nom"/>
+				</div>
+				<label for="prenom" class="col-form-label" placeholder="prenom"></label>
+				<div class="col-md-1">
+					<input type="text" class="form-control" name="prenom"/>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-1 col-form-label"></label>
+				<label for="adresse" class="col-md-2 col-form-label">Adresse postale</label>
+				<div class="col-md-2">
+					<input type="text" class="form-control" name="adresse"/>
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="MDP" class="col-md-4 col-form-label">Mot de passe</label>
-				<div class="col-md-4">
-					<input type="text" class="form-control" name="MDP"/>
-				</div>
+				<label for="mail" class="col-md-1 col-form-label"></label>
+				<label><input type="submit" class="btn btn-primary" value="Inscription" name="button1"></label>
+				<input type="reset" class="btn btn-primary" value="Effacer" name="button2">
 			</div>
-
-				<label for="Num">Numéro Client </label>
-				<input type="text" name="Num" id="Num"/><br>
-				
-				<label for="Prenom">Prénom </label>
-				<input type="text" name="Nom" id="Nom"/><br>
-
-				<label for="Age">Age </label>
-				<input type="number" name="Age" id="Age"/><br>
-						
-				<label for="Classe">Classe de siege </label>
-				<input type="text" name="Classe" id="Classe"/><br>
-
-				<label for="Statut">Statut client </label>
-				<input type="text" name="Statut" id="Statut"/><br>
-
-				<label><input type="submit" value="Soumettre le formulaire"/></label>
-				<input type="reset" value="Effacer"/><br>
  	</form>
 
 </body>
