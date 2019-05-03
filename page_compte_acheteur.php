be0f4214b7448c02f5910fc1b11b2ca4ce1c3063
LyBanjo <gaultier.jousselin@edu.ece.fr>
2019-05-03 16:03:12 +0200

    Removed the -master folder

--- page_compte_acheteur.php
+++ page_compte_acheteur.php
@@ -31,5 +31,60 @@
 			</ul>
 		</div>
 	</nav>
+	
+	<div class="col-md-12">
+		<h2>Votre compte client</h2>
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-11">
+				<!--affichage des ID de connexion de la session en cours-->
+				<?php
+				session_start();
+
+				if(isset($_SESSION['mail']) && isset($_SESSION['MDP'])) {
+					$servername = "localhost";
+					$username ="root";
+					$password = "";
+					$dbname = "piscinedb2";
+					$sql = "";
+
+					$connection = new mysqli($servername, $username, $password, $dbname);
+
+					if($connection->connect_error) {
+						die("Connection failed: " . $connection->connect_error);
+					}
+
+					$sql = "SELECT * FROM acheteur WHERE (mail = '" . $_SESSION['mail'] . "' AND MDP = '" . $_SESSION['MDP'] . "')";
+					$req = mysqli_query($connection, $sql) or die ("Message d'erreur: " . mysqli_error($connection) );
+
+						while($data = mysqli_fetch_assoc($req)) { 
+							echo "ID: ". $data['id']. '<br>';
+							echo "Nom: ". $data['nom']. '<br>';
+							echo "Prénom: ". $data['prenom']. '<br>';
+							echo "Mail: ". $data['mail']. '<br>';
+							echo "Adresse: ". $data['adresse']. '<br>';
+							echo "Image: ". $data['image']. '<br>';
+							echo "Image du mur: ". $data['wallpaper']. '<br>';
+						}
+					}
+					else {
+						echo 'Error ID';
+					}
+
+				?>
+			</div>
+		</div>
+
+
+	<form action="deconnexion.php" method="post">
+			<div class="form-group">
+				<label class="col-md-2 col-form-label">
+					<input type="submit" class ="btn btn-primary" value="Déconnexion">
+				</label>			
+			</div>
+	</form>
+
+	<div>
+
 </body>
 </html>
