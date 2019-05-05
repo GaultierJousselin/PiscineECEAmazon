58d14582d59d9f6d4034e93856e24f4d447fd015
polo4096 <paul.ghazal@edu.ece.fr>
2019-05-05 05:14:10 +0200

    Acceuil + images

--- Accueil.php
+++ Accueil.php
@@ -23,8 +23,40 @@ session_start();
 <body>
 	<?php include 'Navbar.php'; ?>
 
-	<div class="container container-margin">
-		<p>coucou</p>
+	<div class="container m-5 p-5">
+
+<div class="row">
+	<div class="col">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/carrousel1.png" class="d-block w-100" alt="annonce">
+    </div>
+    <div class="carousel-item">
+      <img src="images/carrousel2.png" class="d-block w-100" alt="solde">
+    </div>
+    <div class="carousel-item">
+      <img src="images/carrousel3.png" class="d-block w-100" alt="promo">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</div>
+
+
 	</div>
 	
 	<?php include 'footer.php'; ?>
